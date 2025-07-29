20dfd528636a1b42174955cadb78e1be3771a9d0
eloizalazzaris <164521073+eloizalazzaris@users.noreply.github.com>
2025-07-29 11:49:44 -0300

    Add files via upload

---
+++ missão ia/script.js
@@ -0,0 +1,43 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Durante a aula, o professor perguntou quais profissões podem se beneficiar do pensamento computacional.",
+        alternativas: [
+            "Apenas programadores e engenheiros de software.",
+            "Diversas áreas, como medicina, arquitetura, direito e até arte podem usar o pensamento computacional."
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 4",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 5",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+];
